672bb77f855a3b2d6f420dc1d98b0659a98eb9b8
Kushaal18 <120733272+Kushaal18@users.noreply.github.com>
2023-04-26 14:56:16 +0530

    Update led_blink.ino

--- led_blink.ino
+++ led_blink.ino
@@ -1,21 +1,89 @@
-float Vin; // Input voltage
-float Vout; // Output voltage
-float R1 = 3300; // Resistance of R1 in ohms
-float R2 = 33000; // Resistance of R2 in ohms
-int rawValue; // Raw analog input value
+#include <crt_CleanRTOS.h>
+float Vin; 
+float Vout;
+float Vin1;
+float Vout1; 
+float R1 = 2963; 
+float R2 = 29960; 
+float R3 = 21500;
+float R4 = 2963;
+int rawValue;
+int rawValue1; 
+
+
+void task1(void* parameters) {
+  for (;;) {
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    rawValue = analogRead(2);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    Vout = rawValue * (3.3 / 4096.0);
+    Vin = Vout * ((R1 + R2) / R1);
+    Vin=Vin+1;
+    if (Vin > 23) {
+      digitalWrite(13, HIGH);
+      digitalWrite(15, HIGH);
+      Serial.printf("1,%f,%s", Vin, "HIGH");
+    } else if (Vin > 19.5 && Vin <= 23) {
+      digitalWrite(13, HIGH);
+      digitalWrite(15, LOW);
+      Serial.printf("1,%f,%s",Vin,"HIGH LOW");
+    } else {
+      digitalWrite(13, LOW);
+      digitalWrite(15, LOW);
+      Serial.printf("1,%f,%s",Vin,"LOW");
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void task2(void* parameters) {
+  for (;;) {
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    rawValue1 = analogRead(4);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    Vout1 = rawValue1 * (3.3 / 4096.0);
+    Vin1 = Vout1 * ((R3 + R4) / R4);
+    Vin1=Vin1+4;
+    Serial.println(Vin1);
+    
+    if (Vin1 > 23) {
+      digitalWrite(13, HIGH);
+      digitalWrite(15, HIGH);
+       Serial.printf("2,%f,%s",Vin1,"HIGH");
+    } else if (Vin1 > 19.5 && Vin1 <= 23) {
+      digitalWrite(13, HIGH);
+      digitalWrite(15, LOW);
+      Serial.printf("2,%f,%s",Vin1,"HIGHLOW");
+    } else {
+      digitalWrite(13, LOW);
+      digitalWrite(15, LOW);
+      Serial.printf("2,%f,%s",Vin1,"LOW");
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communication
+  pinMode(13,OUTPUT);
+  pinMode(15,OUTPUT);
+  Serial.begin(9600);
+  xTaskCreate(
+    task1,
+    "task 1",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
+  xTaskCreate(
+    task2,
+    "task 2",
+    1000,
+    NULL,
+    1,
+    NULL
+  );
 }
 
 void loop() {
-  rawValue = analogRead(A0); // Read the raw analog input value
-  Vout = (rawValue / 1023.0) * 3.3; // Convert the raw value to voltage
-  Vin = Vout * (R1 + R2) / R2; // Calculate the input voltage using the voltage divider equation
-  Serial.print("Input voltage: ");
-  Serial.print(Vin, 2); // Print the input voltage with 2 decimal places
-  Serial.print("V, Output voltage: ");
-  Serial.print(Vout, 2); // Print the output voltage with 2 decimal places
-  Serial.println("V");
-  delay(1000); // Wait for 1 second
 }
