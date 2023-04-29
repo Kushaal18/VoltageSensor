84d19c84d020a85f6bc5d81742628660c15b6a1e
Kushaal18 <120733272+Kushaal18@users.noreply.github.com>
2023-04-29 15:45:26 +0530

    Update temperature_sensor.ino

--- temperature_sensor.ino
+++ temperature_sensor.ino
@@ -1,17 +1,35 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 5
+#define ONE_WIRE_BUS 15
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-void setup(){
-  Serial.begin(9600);
+DeviceAddress sensor1 = {};
+DeviceAddress sensor2 = {};
+DeviceAddress sensor3 = {};
+DeviceAddress sensor4 = {};
+
+void setup(void){
+  Serial.begin(115200);
   sensors.begin();
 }
- void loop(){
-  sensors.requestTemperatures();
-  Serial.print("Temperature in celsius:");
-  Serial.println(sensors.getTempCByIndex(0));
+
+void loop(void){ 
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Sensor 1(*C): ");
+  Serial.print(sensors.getTempC(sensor1)); 
+ 
+  Serial.print("Sensor 2(*C): ");
+  Serial.print(sensors.getTempC(sensor2)); 
+  
+  Serial.print("Sensor 3(*C): ");
+  Serial.print(sensors.getTempC(sensor3)); 
+
+  Serial.print("Sensor 4(*C): ");
+  Serial.print(sensors.getTempC(sensor4));
+ 
+  
   delay(1000);
- }
+}
